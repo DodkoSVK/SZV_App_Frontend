2e4b235d669ce4d28b006c765c0e6841e2fa9024
Dominik Chovanec <dominikchovanec@DodBook.local>
2025-09-26 21:51:41 +0200

    Feat(All): Add a Tailwind CSS, Shadcn UI, refactoring home

---
+++ src/components/others/TheNavigation.vue
@@ -0,0 +1,22 @@
+<template>
+    <div className="w-full border-b bg-secondary/30 text-foreground">
+        <div className="container mx-auto flex items-center justify-between py-4 px-6">
+            <RouterLink to="/" className="flex items-center gap-2">
+                <img 
+                    src="@/assets/szv_logo.webp"
+                    alt="SZV Logo"
+                    className="h-15 w-15"
+                />
+                <span className="text-xl font-bold uppercase">SZV Aplikácia</span>
+            </RouterLink>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/TheHome.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+  import TheColorsDemo from '@/components/others/TheColorsDemo.vue';
+</script>
+
+<template>
+  <main>
+    <TheColorsDemo />
+  </main>
+</template>
