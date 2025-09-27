4e1e01e8e61079cdf6f2a48c3e828bf2e87d30dd
Dominik Chovanec <dominikchovanec@DodBook.local>
2025-09-27 16:17:46 +0200

    Feat(ShadUI): Add button component with custom variants

--- src/components/others/TheNavigation.vue
+++ src/components/others/TheNavigation.vue
@@ -9,6 +9,51 @@
                 />
                 <span className="text-xl font-bold uppercase">SZV Aplikácia</span>
             </RouterLink>
+
+            <NavigationMenu>
+                    <NavigationMenuList className="flex items-center gap-4 font-bold">
+                        <NavigationMenuItem>
+                            
+                            <NavigationMenuLink asChild>
+                                <RouterLink to="/">
+                                    Domov
+                                </RouterLink>
+                            </NavigationMenuLink>
+                        </NavigationMenuItem>
+
+                        <NavigationMenuItem>
+                            <NavigationMenuLink asChild>
+                                <RouterLink to="/clubs">
+                                    Kluby
+                                </RouterLink>
+                            </NavigationMenuLink>
+                        </NavigationMenuItem>
+
+                        <NavigationMenuItem>
+                            <NavigationMenuLink asChild>
+                                <RouterLink to="/persons">                                   
+                                    Ľudia
+                                </RouterLink>
+                            </NavigationMenuLink>
+                        </NavigationMenuItem>
+
+                        <NavigationMenuItem>
+                            <NavigationMenuLink asChild>
+                                <RouterLink to="/competitions">
+                                    Súťaže
+                                </RouterLink>
+                            </NavigationMenuLink>
+                        </NavigationMenuItem>
+
+                    </NavigationMenuList>
+                </NavigationMenu>
+
+                <div className="flex items-center gap-4">
+                    <button className="rounded-md border border-input px-3 py-1.5 text-sm font-medium transition-colors hover:bg-primary hover:text-accent-foreground">
+                        Prihlásiť
+                    </button>
+                    <ThemeToggle />
+                </div>
         </div>
     </div>
 </template>
@@ -17,6 +62,3 @@
     import { RouterLink, RouterView } from 'vue-router'
 </script>
 
-<style scoped>
-
-</style>
