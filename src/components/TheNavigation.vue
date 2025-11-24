5c74eca30730050a62d54ad40332f80218cb3076
Dominik Chovanec <dominikchovanec@DodBook.local>
2025-11-24 18:44:11 +0100

    Feat(All): Installed and configured shadcn VUE UI

---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,18 @@
+<template>
+    <div className="container mx-auto flex items-center justify-between py-4 px-6">
+        
+    </div>
+</template>
+
+<script lang="ts">
+    import { 
+        NavigationMenu, 
+        NavigationMenuItem,
+        NavigationMenuLink,
+        NavigationMenuList
+    } from './ui/navigation-menu';
+</script>
+
+<style scoped>
+
+</style>
